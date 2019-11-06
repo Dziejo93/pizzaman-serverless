cc0b3498fa371363e9a104a869b156635daaa1da
Andrzej <andrzej.gatkowski@naturaily.com>
2019-11-06 21:33:55 +0100

    added /dist to gitignore

--- server/dist/config/mongoose.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect(process.env.DB_URI, { useNewUrlParser: true });
-exports.default = mongoose_1.default;
--- server/dist/models/orders/order-model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-exports.default = new mongoose_1.Schema({
-    pizza: String,
-    address: String,
-    orderStatus: String,
-});
